65c013c976725223ae7c55360f5938e8eff9164a
Marcos Gomes <marcosgomesdev@gmail.com>
2024-06-24 05:15:25 -0300

    feat: add dropbox routes

---
+++ src/dropbox/dropbox.controller.ts
@@ -0,0 +1,79 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Query,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Response } from 'express';
+import { Public } from '../decorators/public.decorator';
+import { DropboxService } from './dropbox.service';
+
+@Controller('dropbox')
+export class DropboxController {
+  constructor(private readonly dropboxService: DropboxService) {}
+
+  @Get('/get-folders')
+  async getFolders() {
+    return await this.dropboxService.getFolders();
+  }
+
+  @Public()
+  @Get('/download-file')
+  async downloadFile(@Query('path') path: string, @Res() res: Response) {
+    const file = await this.dropboxService.downloadFile(path);
+
+    console.log(file);
+
+    // Configurar headers para download do arquivo
+    res.setHeader('Content-Disposition', `attachment; filename=${file.name}`);
+    const mimeType = this.getMimeTypeFromFileName(file.name);
+    if (mimeType) {
+      res.setHeader('Content-Type', mimeType);
+    }
+
+    // Enviar conteúdo do arquivo como resposta
+    res.send(file.fileBinary);
+  }
+
+  @Post('/create-folder')
+  async createFolder(@Query('path') path: string) {
+    return await this.dropboxService.createFolder(path);
+  }
+
+  @Post('/upload-file')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(
+    @UploadedFile() file: Express.Multer.File,
+    @Query('path') path: string,
+  ) {
+    const filePath = `/${path}/${file.originalname}`;
+    return await this.dropboxService.uploadFile(filePath, file.buffer);
+  }
+
+  private getMimeTypeFromFileName(filename: string): string | undefined {
+    const ext = filename.split('.').pop()?.toLowerCase();
+    switch (ext) {
+      case 'pdf':
+        return 'application/pdf';
+      case 'jpg':
+      case 'jpeg':
+        return 'image/jpeg';
+      case 'png':
+        return 'image/png';
+      case 'txt':
+        return 'text/plain';
+      case 'mp3':
+        return 'audio/mpeg'; // Exemplo para arquivo MP3
+      case 'wav':
+        return 'audio/wav'; // Exemplo para arquivo WAV
+      case 'ogg':
+        return 'audio/ogg'; // Exemplo para arquivo OGG
+      default:
+        return undefined; // Retorne undefined se o tipo MIME não for conhecido
+    }
+  }
+}
---
+++ src/dropbox/dropbox.service.ts
@@ -0,0 +1,63 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { Dropbox } from 'dropbox';
+
+@Injectable()
+export class DropboxService {
+  private dropbox: Dropbox;
+
+  constructor() {
+    this.dropbox = new Dropbox({
+      accessToken: process.env.ACCESS_TOKEN,
+    });
+  }
+
+  async createFolder(path: string) {
+    if (!path) {
+      throw new BadRequestException('O parâmetro path é obrigatório');
+    }
+
+    try {
+      const response = await this.dropbox.filesCreateFolderV2({
+        path: `/${path}`,
+        autorename: false,
+      });
+      return response;
+    } catch (error) {
+      console.log(error);
+      throw new Error(`Erro ao criar a pasta: ${error.message}`);
+    }
+  }
+
+  async getFolders() {
+    try {
+      const response = await this.dropbox.filesListFolder({
+        path: '',
+      });
+
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async uploadFile(path: string, fileContent: Buffer): Promise<any> {
+    try {
+      const response = await this.dropbox.filesUpload({
+        path: path,
+        contents: fileContent,
+      });
+      return response.result;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async downloadFile(path: string): Promise<any> {
+    try {
+      const response = await this.dropbox.filesDownload({ path });
+      return response.result;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
